1bc76cf9f34ba322929be23021aad727f0eb828e
kybex11 <artm.22@internet.ru>
2024-06-15 17:53:46 +0300

    emae

--- frontend/wailsjs/runtime/runtime.js
+++ frontend/wailsjs/runtime/runtime.js
@@ -1,13 +1,3 @@
-/*
- _       __      _ __
-| |     / /___ _(_) /____
-| | /| / / __ `/ / / ___/
-| |/ |/ / /_/ / / (__  )
-|__/|__/\__,_/_/_/____/
-The electron alternative for Go
-(c) Lea Anthony 2019-present
-*/
-
 export function LogPrint(message) {
     window.runtime.LogPrint(message);
 }
