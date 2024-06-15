bec168a68bd2d89857412a9797df048af9e22fbc
kybex11 <artm.22@internet.ru>
2024-06-15 17:17:18 +0300

    update and remove useless

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.scss';
 
 export default function App() {
     return (
--- main.go
+++ main.go
@@ -8,14 +8,10 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-//go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
-
-	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "KybexEditor",
 		Width:  1024,
