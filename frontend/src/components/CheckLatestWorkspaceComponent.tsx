a782129f70608a794a3904b3c52214407938fc23
kybex11 <artm.22@internet.ru>
2024-06-15 17:51:47 +0300

    update

--- frontend/src/components/CheckLatestWorkspaceComponent.tsx
+++ frontend/src/components/CheckLatestWorkspaceComponent.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import { CheckLatestWorkspace } from '../../wailsjs/go/main/App';
 
-
 export default function CheckLatestWorkspaceComponent() {
-    const latest = CheckLatestWorkspace(); 
+    const [latest, setLatest] = useState<string | null>(null);
 
-    return (
-        <></>
-    )
+    useEffect(() => {
+        async function fetchLatestWorkspace() {
+            const result = await CheckLatestWorkspace();
+            setLatest(result);
+        }
+        fetchLatestWorkspace();
+    })
+
+    if (latest) {
+        return (
+            <div>{latest}</div>
+        )
+    } else {
+        return (
+            <div>Loading...</div> 
+        ) 
+    }
+
 }
