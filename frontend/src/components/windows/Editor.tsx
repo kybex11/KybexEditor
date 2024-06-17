cd456fd6682108e0f1630e305950a0f50e99cfef
kybex11 <artm.22@internet.ru>
2024-06-17 13:06:19 +0300

    plz help me.

--- frontend/src/components/windows/Editor.tsx
+++ frontend/src/components/windows/Editor.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import "../../styles/Editor.scss";
 
 export default function Editor() {
   const [code, setCode] = useState();
 
   return (
-    <></>
+    <>
+    <textarea name="code" id="code_field" title="Enter your code here">
+      test
+    </textarea>
+    </>
   );
 };
