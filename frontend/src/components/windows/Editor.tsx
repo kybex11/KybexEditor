1a908fc6367b70d4c886e59e1f86fb16200a7fce
kybex11 <artm.22@internet.ru>
2024-06-18 10:43:42 +0300

    up

--- frontend/src/components/windows/Editor.tsx
+++ frontend/src/components/windows/Editor.tsx
@@ -3,7 +3,10 @@ import MonacoEditor from '@monaco-editor/react';
 export default function Editor() {
   return (
     <>
-    <MonacoEditor language="javascript" height="500px"/>
+    <div className="editor_">
+      <MonacoEditor language="javascript" height="100vh" width="calc(100vw - 200px)"/>
+    </div>
+    
     </>
   );
 }
